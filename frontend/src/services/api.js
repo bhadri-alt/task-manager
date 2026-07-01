b595ec8a33fd809c2b557faa220346df814245a7
bhadri-alt <bhadribrawlstars@gmail.com>
2026-07-01 10:45:29 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-backend-z8c1.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
