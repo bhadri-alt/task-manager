aefb6a92483c0041e28cd05bd08e00c31727687c
bhadri-alt <bhadribrawlstars@gmail.com>
2026-07-01 11:00:55 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-backend-z8c1.onrender.com/",
+  baseURL: "https://task-manager-backend-z8c1.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
